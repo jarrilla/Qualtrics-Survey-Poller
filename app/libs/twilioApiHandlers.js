41dcf39dd988f31af45929197e8e805578e71712
Jose Arrillaga <jarrilla.services@gmail.com>
2021-09-08 17:48:26 -0400

    updated file format

--- app/libs/twilioApiHandlers.js
+++ app/libs/twilioApiHandlers.js
@@ -12,20 +12,16 @@ const AUTH_TOKEN = process.env.TWILIO_AUTH_TOKEN;
 const CLIENT = require("twilio")(ACCOUNT_SID, AUTH_TOKEN);
 const SOURCE_NUMBER = process.env.TWILIO_PHONE_NUM;
 
-/**
- * Append country code to phone number if none exists
- * @param {string} s phone number string to format
- */
-function formatPhoneNumber(s) {
-  return s.startsWith("+1") ? s : "+1"+s;
-}
+module.exports = {
+  SendMessage
+};
 
 /**
  * Send an SMS
  * @param {string} body the message body to send
  * @param {string} to the phone number to send the message to
  */
-async function sendMessage(body, to) {
+async function SendMessage(body, to) {
   try {
     const sid = await CLIENT.messages.create({
       body: body,
@@ -40,7 +36,10 @@ async function sendMessage(body, to) {
   }
 }
 
-
-module.exports = {
-  sendMessage: sendMessage
-};
+/**
+ * Append country code to phone number if none exists
+ * @param {string} s phone number string to format
+ */
+ function formatPhoneNumber(s) {
+  return s.startsWith("+1") ? s : "+1"+s;
+}
